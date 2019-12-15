c1027e47c47d2d2b8565d8a52b63a12d0934ddc9
Rob Brunstad <robbrunstad@gmail.com>
2019-12-14 23:58:10 -0500

    raw save

--- core_simulator.py
+++ core_simulator.py
@@ -148,14 +148,14 @@ INDICES = [0, 1, 2, 3]
 class Packet:
 	id = 1
 
-	def __init__(self, dx, dy):
+	def __init__(self, parent, dx, dy):
 		self.dx = dx
 		self.dy = dy
 		self.hops = 0	# metrics
 		self.delays = 0	# metrics
 		self.routing_delay = 0
 		self.name = Packet.id
-		self.parent = None
+		self.parent = parent
 		self.ready_to_send = False
 		Packet.id += 1
 
@@ -176,20 +176,18 @@ class Core(Hardware):
 		self.merge_delay = 2
 		Core.id += 1
 
-	def enq(self, packet):
-		self.wait_buffer.append(packet)
-
-	def route(self, packet):
+	def inject(self, packet):
 		packet.routing_delay = self.default_routing_delay
+		packet.parent = self
 		self.wait_buffer.append(packet)
 
-	def old_route(self):
-		'''	Route a packet, either taken directly from the send buffer, or taken
+	def prepare_out(self):
+		'''	Prepare a packet for sendoff, either taken directly from the send buffer, or taken
 			from the wait buffer (provided nothing's been queued in the send buffer)
 		'''
 		# send buffer has higher priority than wait buffer
 		# add unroutable packets back to the buffer
-		to_remove = []
+		new_wait_buffer = deque()
 		new_buffer = deque()
 		while len(self.send_buffer) > 0:
 			packet = self.forward(self.send_buffer.popleft())
@@ -200,21 +198,21 @@ class Core(Hardware):
 		for packet in self.wait_buffer:
 			if packet.routing_delay > 0:
 				packet.routing_delay -= 1
+				new_wait_buffer.append(packet)
 			else:
 				print("Packet " + str(packet.name) + " is at core " + str(self.name) + " and must travel " + str(packet.dx) + " cores in the x and " + str(packet.dy) + " cores in the y")
-				to_remove.append(packet)
 				blocked_packet = self.forward(packet)
 				if blocked_packet:
 					self.send_buffer.append(blocked_packet)
 
-		for packet in to_remove:
-			self.wait_buffer.remove(packet)
+		self.wait_buffer = new_wait_buffer
 
+	def send_out(self):
 		for x in range(0, 4):
 			if self.lines[x] and self.packet_out_buffer[x]:
-				self.lines[x].inject(self.packet_out_buffer[x])
-
-		self.packet_out_buffer = [None, None, None, None]
+				blocked_packet = self.lines[x].inject(self.packet_out_buffer[x])
+				if blocked_packet == None:	# only remove packet if it was successfully forwarded
+					self.packet_out_buffer[x] = None
 
 	def forward(self, packet):
 		''' Adds packets to the appropriate directional output if possible
@@ -254,46 +252,6 @@ class Core(Hardware):
 			return None
 		return packet
 
-
-	def pickup(self):
-		''' Grab new packets from connected lines, but only if they're headed in
-			a direction that this core can help with (i.e. continue sending to
-			the north if packet came from the south, but a core should do nothing
-			if a packet lies to a core's south and has a destination to the south)
-
-			NOT ADHERED TO
-			Merging routes cause packets to be delayed non-deterministically. The
-			following sets of routes are merging:
-				West --> North + South --> North
-				East --> North + South --> North
-
-				North --> South + East --> South
-				North --> South + West --> South
-			Because travel in the x-direction is prioritized over travel in the
-			y-direction, no merging routes exist for packets heading East/West.
-
-		'''
-		random.shuffle(INDICES)
-		for x in INDICES:
-			line = self.lines[x]
-			if line and line.packet and line.packet.routing_delay == 0 and \
-					((line.packet.dx > 0 and x == 2) or \
-					(line.packet.dx < 0 and x == 1)  or \
-					(line.packet.dy > 0 and x == 3) or \
-					(line.packet.dy < 0 and x == 0)):
-				line.packet.routing_delay = self.default_routing_delay
-				if x == 2:
-					line.packet.dx -= 1
-				elif x == 1:
-					line.packet.dx += 1
-				elif x == 3:
-					line.packet.dy -= 1
-				elif x == 0:
-					line.packet.dy += 1
-				self.wait_buffer.append(line.packet)
-				line.packet = None
-
-
 class Line(Hardware):
 	id = 1
 
@@ -306,17 +264,20 @@ class Line(Hardware):
 		self.component_2 = None
 
 	def inject(self, packet):
-		self.packet = packet
-		self.packet.routing_delay = self.default_routing_delay
+		if self.packet == None:
+			self.packet = packet
+			packet.parent = self
+			self.packet.routing_delay = self.default_routing_delay
+			return None
+		else:
+			return packet
 
 	def connect(self, terminus_1, terminus_2):
 		self.component_1 = terminus_1
 		self.component_2 = terminus_2
 
-	def route(self):
-		if self.packet:
-			if self.packet.routing_delay > 0:
-				self.packet.routing_delay -= 1
+	def dissassociate(self):
+		self.packet = None
 
 def simulate(packets, timesteps):
 	'''	Make each component perform its duty per timestamp
@@ -341,16 +302,13 @@ def simulate(packets, timesteps):
 				for each packet in the send buffer:
 					add to directional packet_out register if register is empty
 							else return to send buffer
-					packet.ready_to_send = True
 				for each packet in the wait buffer:
 					add to directional packet_out register if register is empty
 							else send to send buffer
-					packet.ready_to_send = True
-		for each packet, selected at random:
-			if packet.ready_to_send:
+		for each core in to_visit, selected randomly:
+			for each packet in the packet_out_buffer:
 				offload packet to corresponding wire
-				packet.ready_to_send = False
-				packet.parent_component.packet_out_buffer[packet] = None
+				packet.parent_component.packet_out_buffer[packet] = wire
 
 		These four steps need to be computed discretely for each core because
 		otherwise, for a given core c, whether a wire w is cleared before a
@@ -362,36 +320,23 @@ def simulate(packets, timesteps):
 	to_visit = []
 	for packet in packets:
 		if isInstance(packet.parent, Line) and packet.routing_delay == 0:
+			packet.parent.dissassociate()
 			if packet.dx > 0:
-				packet.parent.component_2.route(packet)
+				packet.parent.component_2.inject(packet)
 			elif packet.dx < 0:
-				packet.parent.component_1.route(packet)
+				packet.parent.component_1.inject(packet)
 			elif packet.dy > 0:
-				packet.parent.component_1.route(packet)
+				packet.parent.component_1.inject(packet)
 			elif packet.dy < 0:
-				packet.parent.component_2.route(packet)
+				packet.parent.component_2.inject(packet)
 		if isInstance(packet.parent, Core) and packet.parent_component not in to_visit:
 			to_visit.append(packet.parent)
+	for core in to_visit:
+		core.prepare_out()
+	random.shuffle(to_visit)
+	for core in to_visit:
+		core.send_out()
 
-
-	#for t in range(0, timesteps):
-	#	print("t = " + str(t))
-	#	visited = {}
-	#	for x in range(0, len(core_array)):
-	#		for y in range(0, len(core_array[0])):
-	#			core = core_array[x][y]
-	#			for line in core.lines:
-	#				if line and not visited.get(line):
-	#					line.route()
-	#					visited[line] = True
-	#	for x in range(0, len(core_array)):
-	#		for y in range(0, len(core_array[0])):
-	#			core = core_array[x][y]
-	#			core.pickup()
-	#	for x in range(0, len(core_array)):
-	#		for y in range(0, len(core_array[0])):
-	#			core = core_array[x][y]
-	#			core.route()
 
 core_array = []
 size = 16
@@ -414,26 +359,27 @@ for x in range(0, size - 1):
 		core_array[x][y].lines[3].connect(core_array[x][y], core_array[x][y + 1])
 
  # These packets intersect at core 1, 1 and neither gets delayed because their routing pipelines do not overlap
-core_array[0][1].send_buffer.append(Packet(0, -5))
-core_array[1][0].send_buffer.append(Packet(4, -1))
+packets = []
+packets.append(Packet(core_array[0][1], 0, -5))
+packets.append(Packet(core_array[1][0], 4, 1))
 
  # Only one of these packets can be sent east first
-core_array[0][0].send_buffer.append(Packet(4, -1))
-core_array[0][0].send_buffer.append(Packet(4, -1))
+packets.append(Packet(core_array[0][0], 4, -1))
+packets.append(Packet(core_array[0][0], 4, -1))
 
  # Only one of these packets can be sent west first
-core_array[4][4].send_buffer.append(Packet(-2, -2))
-core_array[4][4].send_buffer.append(Packet(-1, -2))
+packets.append(Packet(core_array[4][4], -2, -2))
+packets.append(Packet(core_array[4][4], -1, -2))
 
  # These packets meet at an intermediate core but do not conflict because their routing pipelines do not overlap
-core_array[2][10].send_buffer.append(Packet(0, -5))
-core_array[4][10].send_buffer.append(Packet(0, 3))
+packets.append(Packet(core_array[2][10], 0, -5))
+packets.append(Packet(core_array[4][10], 0, 3))
 
  # These packets meet at an intermediate core but do experience a merge delay
-core_array[10][9].send_buffer.append(Packet(1, -5))
-core_array[9][10].send_buffer.append(Packet(0, -5))
+packets.append(Packet(core_array[10][9], 1, -5))
+packets.append(Packet(core_array[9][10], 0, -5))
 
- # This packet attempts to route off the edge of the grid and is destroyed
-core_array[15][14].send_buffer.append(Packet(5, -5))
+for packet in packets:
+	packet.parent.inject(packet)
 
-simulate(core_array, 200)
+simulate(packets, 200)
